c87e7aebf8d5c9fd3331bccc6eb46a6f3372c82d
Ashish Sundriyal <ashishsundriyal@cdac.in>
2024-11-27 17:43:28 +0530

    demo program-1

---
+++ 03_Input_and_Output_Statements/03_DEMO_PROGRAM_1_read_input_data_from_the_user.py
@@ -0,0 +1,15 @@
+# --------------------------------------------------------------------------------------------------
+# ------------ Demo Program 1 - Read Input data from the user ----------------------
+# --------------------------------------------------------------------------------------------------
+
+'''
+x = input("Enter first number : ")
+y = input("Enter second number : ")
+i=int(x)
+j=int(y)
+print("SUM = ", i+j)
+'''
+
+x = int(input("Enter first number : "))
+y = int(input("Enter second number : "))
+print("SUM = ", x+y)
