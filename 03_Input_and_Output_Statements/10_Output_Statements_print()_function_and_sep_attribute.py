4d74f21e54176448c9a2adde34e8c070776ff708
Ashish Sundriyal <ashishsundriyal@cdac.in>
2024-12-04 17:33:16 +0530

    Section-03 Output-Statements

---
+++ 03_Input_and_Output_Statements/10_Output_Statements_print()_function_and_sep_attribute.py
@@ -0,0 +1,71 @@
+# --------------------------------------------------------------------------------------------------
+# ------------ Output Statements : print() function and sep attribute ------------------------------
+# --------------------------------------------------------------------------------------------------
+
+# ---------------------------------------------------
+# -------- print() function -----------
+# ---------------------------------------------------
+
+# ---------------------------------------------------
+# Case-1 : print() without any arguments
+# ---------------------------------------------------
+print("Ashish")
+print() # gives empty/blank line in between of Ashish and Sundriyal
+print("Sundriyal")
+
+
+# ---------------------------------------------------
+# Case-2 : print(String)
+# ---------------------------------------------------
+
+# New line = \n
+print("Ashish\nSundriyal") 
+# OUTPUT: Ashish
+#         Sundriyal
+
+# Tab character = \t
+print("Ashish\tSundriyal") # OUTPUT: Ashish  Sundriyal
+
+# String concatenation = +
+print("Ashish"+"Sundriyal") # OUTPUT: AshishSundriyal
+
+# String multiplied no of times of a number
+print(10*"Ashish") # OUTPUT: AshishAshishAshishAshishAshishAshishAshishAshishAshishAshish
+
+
+# ---------------------------------------------------
+# Case-3 : assign and print multiple values together
+# ---------------------------------------------------
+a,b,c = 10,20,30
+print("Values are : ", a,b,c)
+# It have automatic-spaces between them
+
+
+
+# ---------------------------------------------------
+# Case-4 : sep attribute
+# ---------------------------------------------------
+a,b,c,d = 10,20,30,40
+print(a,b,c,d) # OUTPUT => 10 20 30 40
+print(a,b,c,d, sep=":") # OUTPUT => 10:20:30:40
+
+
+# ---------------------------------------------------
+# Case-5 : end attribute
+# ---------------------------------------------------
+print("---------- WITHOUT end attribute ---------------------")
+print("Hello")
+print("Ashish")
+print("Sundriyal")
+print("---------- WITH end attribute ------------------------")
+print("Hello", end=" ")
+print("Ashish", end=" ")
+print("Sundriyal")
+print()
+print("Hello", end="::")
+print("Ashish", end="$$$")
+print("Sundriyal")
+
+
+# Continue from 9:00 end attribute
+
