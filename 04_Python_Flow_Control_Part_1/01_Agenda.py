0880d5d2b1302ac78a8be6b1dca0a36dc277aa33
Ashish Sundriyal <ashishsundriyal@cdac.in>
2024-12-10 18:29:37 +0530

    flow control

---
+++ 04_Python_Flow_Control_Part_1/01_Agenda.py
@@ -0,0 +1,32 @@
+# --------------------------------------------------------------------------------------------------
+# ----------------------- Python Flow Control ----------------------------------------------
+# ------------------------------ Part-1 ----------------------------------------------------
+# --------------------------------------------------------------------------------------------------
+
+# ------------------ AGENDA -----------------------
+
+
+# 1. Selection statements (if, if-else, if-elif-else)
+# 2. Iterative Statements (for, while)
+# 3. Transfer Statements (break, continue)
+
+# Extra : pass, del
+
+# There is no switch statement in python
+# There is no do-while loop in python
+# There is no goto statement in python
+
+# Python  : Indentation is important (just like paranthesis of other languages)
+# Example
+'''
+if condition:
+    Statement-1
+    Statement-2
+    Statement-3
+Statement-4
+'''
+'''
+if(10<20):
+print("GOOD")   IndentationError
+print("OUT")
+'''
