001c0fb810302f6781a76b1a4f24c709f51de432
Ashish Sundriyal <ashishsundriyal@cdac.in>
2024-12-16 14:52:40 +0530

    lec07

---
+++ 04_Python_Flow_Control_Part_1/07_While_loop_with_applications.py
@@ -0,0 +1,42 @@
+# --------------------------------------------------------------------------------------------------
+# ----------------------- While loop ------------------------------------------
+# --------------------------------------------------------------------------------------------------
+# for every element, present in the sequence (list, tuple, set, dict, string, range), 
+# if you want to perform any activity, then we go for => FOR LOOP
+
+# SYNTAX : 
+# while condition:
+    # do_any_activity
+
+# # Example : 
+# i=1
+# while(i<=10):
+#     print(i)
+#     i+=1
+
+# No of 1 to 20 which are divisible by 3
+# x=1
+# while(x<=20):
+#     if(x%3==0):
+#         print(x)
+#     x+=1
+
+
+# n = int(input("Enter number : "))
+# sum=0
+# i=1
+# while(i<=n):
+#     sum=sum+i
+#     i+=1
+# print(sum)
+
+
+
+name=""
+while(name!="sunny"):
+    name=input("Enter name : ")
+print("NAME = ", name)
+
+
+
+
