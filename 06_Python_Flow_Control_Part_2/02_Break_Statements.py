bc778e07061491e88bfb75bb627ced0e8c8c9fac
Ashish Sundriyal <ashishsundriyal@cdac.in>
2024-12-17 18:18:33 +0530

    lec02

---
+++ 06_Python_Flow_Control_Part_2/02_Break_Statements.py
@@ -0,0 +1,14 @@
+# -----------------------------------------------------------------------------
+# ------------ Break Statement ---------------
+# -----------------------------------------------------------------------------
+
+
+# EXAMPLE : 
+
+for i in range(11):
+    if i==7:
+        print("Processing is Enough, Break loop code execution........")
+        break
+    else:
+        print("i=>",i)
+print("outside loop")
