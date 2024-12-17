280a4b03018dc263583069ed17d3f7bd6da506ec
Ashish Sundriyal <ashishsundriyal@cdac.in>
2024-12-17 18:30:51 +0530

    lec02 update

--- 06_Python_Flow_Control_Part_2/02_Break_Statements.py
+++ 06_Python_Flow_Control_Part_2/02_Break_Statements.py
@@ -1,8 +1,17 @@
 # -----------------------------------------------------------------------------
 # ------------ Break Statement ---------------
 # -----------------------------------------------------------------------------
-
-
+# IF YOU WANT TO USE BREAK STATEMENT, YOU SHOULD ALWAYS USE IT WITHIN THE LOOP
+# OUTSIDE THE LOOP, IT WON'T WORK 
+# GIVES SYNTAX ERROR
+'''
+i=2
+if i==7:
+    print("Processing is Enough, Break loop code execution........")
+    break # SyntaxError: 'break' outside loop
+else:
+    print("i=>",i)
+'''
 # EXAMPLE : 
 
 for i in range(11):
@@ -12,3 +21,13 @@ for i in range(11):
     else:
         print("i=>",i)
 print("outside loop")
+
+
+
+
+cart=[10,20,30,600,70,80]
+for item in cart:
+    if item>500:
+        print("To place this order, insurance is required")
+        break
+    print('Processing Item:',item)
