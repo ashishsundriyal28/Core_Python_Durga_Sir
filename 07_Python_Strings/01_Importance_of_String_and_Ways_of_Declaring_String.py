55c567f2ac8d500ac335a69742b7c247db934375
Ashish Sundriyal <ashishsundriyal@cdac.in>
2025-01-09 16:43:40 +0530

    section 07 Python Strings

---
+++ 07_Python_Strings/01_Importance_of_String_and_Ways_of_Declaring_String.py
@@ -0,0 +1,23 @@
+# --------------------------------------------------------------------------------------------------
+# ------------------------------- Python Strings --------------------------------------
+# ---------------- Importance of String and Ways of Declaring String ------------------
+# --------------------------------------------------------------------------------------------------
+
+# ------------------ AGENDA -----------------------
+# To practice problems regarding Strings
+
+
+s = "Ashish"
+print(s)
+
+# how to add double and single quotes in your string
+
+# enclose the string in triple-quotes (easiest way)
+str = '''We can use both " double  and single' quotes'''
+print(str)
+
+
+# use escape characters
+str = ' We should use \' for backslash \" double '
+print(str)
+
---
+++ 07_Python_Strings/02_How_to_access_characters_of_String_and_application.py
@@ -0,0 +1,37 @@
+# --------------------------------------------------------------------------------------------------
+# ------------------------------- Python Strings --------------------------------------
+# --------------------------------------------------------------------------------------------------
+
+
+# ------------------------------ AGENDA -----------------------------------
+# How to access characters of strings and applications
+
+
+# 1. By using index
+
+# +ve index = from left to right, starts from 0
+# -ve index = from right to left, starts from -1
+
+s = "Ashish"
+print(s[0], " ", s[1])
+print(s[-1], " ", s[-2])
+
+
+
+s = "durga"
+i=0
+for x in s:
+    print(f'''The character at +ve index {i} and -ve index {i-len(s)} is {x}''')
+    i=i+1
+
+
+
+
+
+
+
+
+
+
+
+
