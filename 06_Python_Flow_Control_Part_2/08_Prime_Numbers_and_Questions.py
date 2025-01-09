aff6cf0d22e120ad435e4ed75812b22459e78194
Ashish Sundriyal <ashishsundriyal@cdac.in>
2025-01-09 16:42:34 +0530

    update 08

--- 06_Python_Flow_Control_Part_2/08_Prime_Numbers_and_Questions.py
+++ 06_Python_Flow_Control_Part_2/08_Prime_Numbers_and_Questions.py
@@ -7,7 +7,7 @@
 # -----------------------------------------------------------------------------
 # Ques 1 : WAP to check whether a given number is prime or not
 # -----------------------------------------------------------------------------
-
+'''
 n = int(input("Enter number : "))
 
 if n<=1:
@@ -24,13 +24,15 @@ else:
         print("Prime")
     else:
         print("NP")
-
+'''
 
 
 # ******************************************************
 # Optimised verison : to reduce the no of iterations
 # ******************************************************
 
+
+'''
 if n<=1:
     print("Less than one")
 
@@ -45,7 +47,7 @@ else:
         print("Prime")
     else:
         print("NP")
-
+'''
 
 
 
@@ -55,16 +57,48 @@ else:
 # --------------------------------------------------------------------------------------------
 
 
+'''
+n = int(input("Enter number : "))
+n1=2
+
+while n1<=n:
+    # code to check if n1 is prime or not
+    is_prime=True
+    for i in range(2,n1//2 + 1):
+        if n1%i==0:
+            is_prime=False
+            break
+    if is_prime==True:
+        print("n1 = ", n1)
+    n1=n1+1
+'''
 
 
 
 
 
 
+# --------------------------------------------------------------------------------------------
+# Ques 3 : WAP to generate first n prime numbers 
+# --------------------------------------------------------------------------------------------
 
+n = int(input("Enter number : "))
+count=0
+n1=2
 
-
-
+while True:
+    # code to check if n1 is prime or not
+    is_prime=True
+    for i in range(2,n1//2 + 1):
+        if n1%i==0:
+            is_prime=False
+            break
+    if is_prime==True:
+        print("n1 = ", n1)
+        count = count+1
+    if count == n:
+        break
+    n1=n1+1
 
 
 
